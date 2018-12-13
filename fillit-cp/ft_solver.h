fe2b36fcb4a47fe66d3566e4c0ad1de3f360b128
frolushka <mpfrolovstud@gmail.com>
2018-12-13 12:13:34 +0300

    Almost done 2

--- fillit-cp/ft_solver.h
+++ fillit-cp/ft_solver.h
@@ -6,19 +6,19 @@
 /*   By: sbednar <sbednar@student.fr.42>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 12:50:34 by edraugr-          #+#    #+#             */
-/*   Updated: 2018/12/13 12:06:26 by sbednar          ###   ########.fr       */
+/*   Updated: 2018/12/13 12:13:10 by sbednar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_SOLVER_H
 # define FT_SOLVER_H
 
-# define USAGE_MSG "usage: ./fillit source_file"
-
 # include "ft_dlist.h"
 # include "ft_matrix.h"
 # include "ft_matrix_figs.h"
 # include "ft_reader.h"
+
+# define USAGE_MSG "usage: ./fillit source_file"
 
 int		get_square_size(int const n);
 int		find_place(t_dlist *fg, t_matrix **m, int ofs);

9b16295137eddd42cd6879dc4c707f63b0e2298d
Seeker Bednar <mpfrolovstud@gmail.com>
2019-02-06 12:16:57 +0300

    [FIX] fdf: almost work iso

--- fdf/ll3d/src/dots/dots_fill_cb.c
+++ fdf/ll3d/src/dots/dots_fill_cb.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dots_fill_cb.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbednar <sbednar@student.fr.42>            +#+  +:+       +#+        */
+/*   By: sbednar <sbednar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 07:12:44 by sbednar           #+#    #+#             */
-/*   Updated: 2019/02/06 11:15:02 by sbednar          ###   ########.fr       */
+/*   Updated: 2019/02/06 12:12:07 by sbednar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,28 +44,28 @@
 
 void			dots_fill_cb(const t_vec4 *p, const t_mtx4 *c, t_vec3 *res)
 {
+	// float	kx;
+	// float	ky;
+	// float	kz;
+	// float	d;
 	// t_mtx4	*inv;
 	// float	det;
 
-	// inv = mtx4_init(0);
-	// det = dots_fill_cb_det(c->val);
-	// dots_fill_cb_inv(c->val, inv->val, det);
-	// dots_fill_cb_res(inv->val, p, res);
 	t_vec3	*av = vec3_new(c->val[3][0], c->val[3][1], c->val[3][2]);
 	t_vec3	*bv = vec3_new(c->val[3][0] + c->val[0][0], c->val[3][1] + c->val[0][1], c->val[3][2] + c->val[0][2]);
 	t_vec3	*cv = vec3_new(c->val[3][0] + c->val[1][0], c->val[3][1] + c->val[1][1], c->val[3][2] + c->val[1][2]);
 
 	float kx = (bv->y - av->y) * (cv->z - av->z) - (bv->z - av->z) * (cv->y - av->y);
 	float ky = (bv->z - av->z) * (cv->x - av->x) - (bv->x - av->x) * (cv->z - av->z);
-	float kz = (bv->y - av->y) * (cv->x - av->x) - (bv->x - av->x) * (cv->y - av->y);
+	float kz = -(bv->y - av->y) * (cv->x - av->x) + (bv->x - av->x) * (cv->y - av->y);
 	float d = -av->x * (bv->y - av->y) * (cv->z - av->z) -
-		-av->x * (bv->z - av->z) * (cv->x - av->x) +
+		-av->x * (bv->z - av->z) * (cv->y - av->y) +
 		-av->y * (bv->z - av->z) * (cv->x - av->x) -
-		-av->y * (bv->x - av->x) * (cv->z - av->z) +
-		-av->z * (bv->y - av->y) * (cv->x - av->x) -
+		-av->y * (bv->x - av->x) * (cv->z - av->z) -
+		-av->z * (bv->y - av->y) * (cv->x - av->x) +
 		-av->z * (bv->x - av->x) * (cv->y - av->y);
 
-	float l = (kx * p->x + ky * p->y + kz * p->z + d) / -(kx * kx + ky * ky + kz * kz);
+	float l = -(kx * p->x + ky * p->y + kz * p->z + d) / (kx * kx + ky * ky + kz * kz);
 	res->x = p->x + l * kx - av->x;
 	res->y = p->y + l * ky - av->y;
 	res->z = p->z + l * kz - av->z;
